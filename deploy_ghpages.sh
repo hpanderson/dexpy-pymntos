5a7e32a5b1cab0012e30de13c46f3940e8bf02b1
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-02 10:26:23 -0500

    Changed deploy script to operate on a separate clone.
    This avoids deploy_ghpages.sh from being copied to the gh-pages
    branch due to it being in use when cleaning.
    
    It also prevents wiping out uncommitted files on the master when
    the script is called.

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -10,24 +10,30 @@ jupyter nbconvert --to slides dexpy-demo.ipynb --reveal-prefix=reveal.js
 
 # save off files we need for gh-pages
 mkdir -p /tmp/workspace
-cp dexpy-demo.slides.html /tmp/workspace/index.html
+mv dexpy-demo.slides.html /tmp/workspace/index.html
 cp -r reveal.js /tmp/workspace
 
+# create a folder for gh-pages branch
+mkdir -p /tmp/gh-pages
+cp -r .git /tmp/gh-pages
+
 # switch to gh-pages and remove everything
+cd /tmp/gh-pages
+# could delete the gh-pages branch here, if we wanted to keep it at 1 commit
+# not sure if there's an advantage to having a history for it
 git checkout -fB gh-pages
 git rm -rf .
 git checkout master .gitignore
 git reset HEAD
 
-cp -r /tmp/workspace/* .
-
 # add and commit
+cp -r /tmp/workspace/* .
 touch .nojekyll
 git add -A
 git rm .gitignore -f
 git commit -m "Generated gh-pages for $(git log master -1 --pretty=short --abbrev-commit)"
 git push --force --quiet origin gh-pages
 
-# switch back to master
-git checkout master
+# clean up
 rm -rf /tmp/workspace
+rm -rf /tmp/gh-pages
