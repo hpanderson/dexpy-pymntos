0433f812b00b134eb1142a46658ae5c4677ecd5d
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-09 13:24:02 -0600

    Execute notebook before copying to gh-pages.

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,7 +6,8 @@ export DEXPY_ENV_ROOT=$CONDA_ROOT/envs/$DEXPY_ENV_NAME
 export PATH=$DEXPY_ENV_ROOT/bin:$CONDA_ROOT/bin:$PATH
 
 source activate $DEXPY_ENV_NAME
-jupyter nbconvert --to slides dexpy-demo.ipynb --template output_toggle --reveal-prefix=reveal.js --ClearOutputPreprocessor.enabled=True --ExecutePreprocessor.enabled=True
+jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True --ExecutePreprocessor.enabled=True
+jupyter nbconvert --to slides dexpy-demo.ipynb --template output_toggle --reveal-prefix=reveal.js
 
 # save off files we need for gh-pages
 mkdir -p /tmp/workspace
