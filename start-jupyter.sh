f9159617b42fc764f143b4c5f54e7d3b907f7016
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-10 19:52:12 +0000

    Generated gh-pages for commit 7f21d50 Author: Hank Anderson <hank.p.anderson@gmail.com>
        Moved the plot_shift function down to the power graphs.

--- start-jupyter.sh
+++
@@ -1,8 +0,0 @@
-export CONDA_ROOT=/opt/conda
-export DEXPY_ENV_NAME=dexpy_env
-export DEXPY_ENV_ROOT=$CONDA_ROOT/envs/$DEXPY_ENV_NAME
-export PATH=$DEXPY_ENV_ROOT/bin:$CONDA_ROOT/bin:$PATH
-source activate $DEXPY_ENV_NAME
-ln -s /vagrant/dexpy-pymntos/dexpy-demo.ipynb /vagrant/notebook/dexpy-demo.ipynb
-cp /vagrant/dexpy-pymntos/img/* /vagrant/notebook/img
-jupyter notebook --notebook-dir=/vagrant/notebook --no-browser --ip=0.0.0.0 &
